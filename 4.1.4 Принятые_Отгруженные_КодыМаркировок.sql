93ee77bedaa76faa1396a56df8fe3c3816ce0e72
Artem Borovik <borovikao@mail.ru>
2023-06-06 18:01:36 +0600

    Create 4.1.4 Принятые_Отгруженные_КодыМаркировок.sql

---
+++ 4.1.4 Принятые_Отгруженные_КодыМаркировок.sql
@@ -0,0 +1,69 @@
+if OBJECT_ID('GetDeliveryResponseMarks', 'P') is not null
+	drop procedure GetDeliveryResponseMarks
+	
+
+CREATE PROC GetDeliveryResponseMarks
+
+	@DocNum nvarchar(50),
+	@Update bit = 0
+
+as
+
+begin
+		
+select
+	null as ExternalCode,
+    tdrm.DeliveryRequestCode as DeliveryRequestCode,  
+    null as DeliveryRequestRowCode,
+    null as KI,
+	null as KIGU,
+	null as KITU,
+	null as SSCC,
+    tdrm.PROCESSED_COMMENT as Comment,
+	tdrm.DOCNUM
+from
+    tbl_DeliveryResponseMarks tdrm 
+where 
+    tdrm.DOCNUM = @DocNum
+group by
+    tdrm.DeliveryRequestCode,
+    tdrm.PROCESSED_COMMENT,
+	tdrm.DOCNUM
+HAVING
+	MAX(IIF(tdrm.CHECKED_BY_PI is null, 0, 1)) = @Update	
+
+union
+
+select
+	tdrm.ExternalCode,
+    tdrm.DeliveryRequestCode as DeliveryRequestCode,  
+    tdrm.DeliveryRequestRowCode,
+    tdrm.KI,
+	tdrm.KIGU,
+	tdrm.KITU,
+	tdrm.SSCC,
+    tdrm.PROCESSED_COMMENT as Comment,
+	tdrm.DOCNUM
+from
+    tbl_DeliveryResponseMarks tdrm 
+where 
+    tdrm.DOCNUM = @DocNum
+group by
+	tdrm.ExternalCode,
+    tdrm.DeliveryRequestCode,  
+    tdrm.DeliveryRequestRowCode,
+    tdrm.KI,
+	tdrm.KIGU,
+	tdrm.KITU,
+	tdrm.SSCC,
+    tdrm.PROCESSED_COMMENT,
+	tdrm.DOCNUM
+HAVING
+	MAX(IIF(tdrm.CHECKED_BY_PI is null, 0, 1)) = @Update
+order by
+	DeliveryRequestCode, ExternalCode
+end;
+
+
+	
+	
